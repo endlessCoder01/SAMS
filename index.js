15052a4a5f5f4f47d354f3b6305d55c5421209ff
munashe <munashemudoti@gmail.com>
2025-06-27 21:51:51 +0200

    jwt sign working

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const app = express();
 
-app.use(express(JSON));
+app.use(express.json());
 app.use(cors());
 
 const posts = [
@@ -23,9 +24,13 @@ app.get("/posts", (req, res) => {
   res.json(posts);
 });
 
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
     //authenticate user using bcrypt
 
+    const username = req.body.username
+    const user = {name: username}
+    const accessToken = jwt.sign(user, process.env.JWT_SECRET)
+    res.json({accessToken: accessToken})
 })
 
 app.listen(3000);
