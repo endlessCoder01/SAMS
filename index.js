80967c2e7dda6cce43742238b7a21dcf6a5a6cf5
munashe <munashemudoti@gmail.com>
2025-06-27 18:57:49 +0200

    implementing webtokens

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const jwt = require("jsonwebtoken");
 
 const app = express();
-
 
 app.use(express(JSON));
 app.use(cors());
 
-app.listen(3000)
+const posts = [
+  {
+    username: "Munashe",
+    title: "Post 1",
+  },
+  {
+    username: "Mike",
+    title: "Post 2",
+  },
+];
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.get('/login', (req, res) => {
+    //authenticate user using bcrypt
+
+})
+
+app.listen(3000);
