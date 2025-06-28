83b70724dc201c131cc140979dcbb3c93f4edf53
munashe <munashemudoti@gmail.com>
2025-06-28 07:00:51 +0200

    authorization success

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const posts = [
   },
 ];
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name));
 });
 
 app.post('/login', (req, res) => {
@@ -32,5 +32,19 @@ app.post('/login', (req, res) => {
     const accessToken = jwt.sign(user, process.env.JWT_SECRET)
     res.json({accessToken: accessToken})
 })
+
+function authenticateToken(req, res, next){
+//Bearer Token
+const authHeader = req.headers['authorization']
+const token = authHeader && authHeader.split(' ')[1]
+
+if(token == null)return res.sendStatus(401)
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+    next()
+    })
+
+}
 
 app.listen(3000);
