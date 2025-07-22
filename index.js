b7ae4895d682d4ff4811d19738b597022458f1a8
munashe <munashemudoti@gmail.com>
2025-07-22 06:30:39 +0200

    multer done

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const path = require("path");
+const multer = require("multer"); 
+
 require("dotenv").config();
 
 const app = express();
@@ -39,6 +42,36 @@ app.use('/alert', alertRoutes);
 app.use('/test', testRoutes);
 app.use('/task', taskRoutes);
 app.use('/farm-member', farm_memberRoutes);
+
+
+// Set up storage for multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = "uploads"; // Directory to save images
+    fs.mkdirSync(uploadDir, { recursive: true }); // Create directory if it doesn't exist
+    cb(null, uploadDir); // Set destination
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Save file with timestamp
+  },
+});
+
+// Initialize multer with the defined storage
+const upload = multer({ storage: storage });
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/Documents", express.static(path.join(__dirname, "Documents")));
+
+app.post("/uploads", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  const filePath = path.join("uploads", req.file.filename); // Get the file path
+  res.json({ path: filePath }); // Return the file path as JSON
+});
+
+app.get("/", (req, res) => {
+  res.send("Documents"); // Response for the root URL
+});
 
 
 const PORT = process.env.PORT;
